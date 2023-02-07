0db19bd40466350e835d1723e307362d9b5c022c
Kukukachuj <124608754+Kukukachuj@users.noreply.github.com>
2023-02-07 11:32:29 +0700

    Create Messingaround.py

---
+++ Messingaround.py
@@ -0,0 +1,14 @@
+print ('hi stupid')
+f_name = input('Whats your first name: ')
+l_name = input('How about a last name: ')
+born_month = input(' What day of the month were you born?: ')
+born_day = input('And the month?: ')
+born_year = input('year?: ')
+print('Hey ',f_name,l_name,' so your birthday is ',born_month, ' / ',born_day, '/',born_year)
+from datetime import date
+today = date.year 
+print (date.year )
+calculate_age = today.year - born_year - ((today.month,today.day) < (born_month,born._day))
+today = date.today
+ 
+ 
