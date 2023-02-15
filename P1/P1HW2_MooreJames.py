1c626cb1211f4fcf9283884b9da41281ba9b36bf
Kukukachuj <124608754+Kukukachuj@users.noreply.github.com>
2023-02-15 16:30:51 -0500

    Final Draft

--- P1/P1HW2_MooreJames.py
+++ P1/P1HW2_MooreJames.py
@@ -1,15 +1,19 @@
+#A program that takes in a users Trip exspenses and budget and displays them while subtracting the exspenses from thier budget. 
+#Date 02-12-2023
+#CTI-110P1HW2 - Travel Expsense 
+# James Moore
 budget=int(input("Enter the budget:"))  #Users Total Budget Input
-destination=input("Enter Your destination:")  #Users Desintaion Input
-fuel=float(input("Enter amount that you will spend on gas:"))  #Users Gas Expenses Input
-accomodation=float(input("Enter amount that you will spend on accomodation:")) #User Accomodation Input
-food=float(input("Enter amount that you will spend on food:")) #Users Food Expenses Input
+destination=input("Enter Your destination:")  #Users Desintaion Input``
+fuel=int(input("Enter amount that you will spend on gas:"))  #Users Gas Expenses Input
+accomodation=int(input("Enter amount that you will spend on accomodation:")) #User Accomodation Input
+food=int(input("Enter amount that you will spend on food:")) #Users Food Expenses Input
 expenses=fuel+accomodation+food #Addintion of all Expenses  
-print("----------------Travel Exspenses-----------------") 
-print("Location: ",destination)
-print("initial Budget: ",budget)
-print (end='')
-print("fuel: ",fuel)
-print("Accomodations: ",accomodation)
-print("Food: ",food)
-print(end='')
-print("Remaining Balance: ",budget-expenses)
+print("----------------Travel Exspenses-----------------") #Prints Title bar
+print("Location:",destination) #Prints User Location
+print("Initial Budget:",budget) #Prints Starting Budget
+print ("") #Skips Line
+print("Fuel: ",fuel)#Prints fuel costs
+print("Accomodations:",accomodation)#Prints accomodations
+print("Food:",food)#Prints food costs
+print("")#Skips line
+print("Remaining Balance:",budget-expenses)#Prints Remaining balance 
