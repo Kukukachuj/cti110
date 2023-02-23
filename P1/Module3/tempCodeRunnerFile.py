63b0ae8186cb398f86ec2b5a0aa23d6d2467de8f
Kukukachuj <124608754+Kukukachuj@users.noreply.github.com>
2023-02-23 17:48:25 -0500

    New

--- P1/Module3/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-user_num = int(input('Enter integer:\n'))
-print('You entered:',user_num)
-print(user_num,'squared is',user_num ** 2 ) 
-print('And',user_num,'cubed is',user_num ** 3,'!!')
-user_num2 = int(input('Enter another integer:\n'))
-print(user_num,'+',user_num2,'is',(user_num + user_num2)) 
-print(user_num,'*',user_num2,'is',(user_num * user_num2))
---
+++ P1/Module4/P2LAB1_MooreJames.py
@@ -0,0 +1,8 @@
+user_num = int(input())
+div_num = int(input())
+
+an_1 = user_num//div_num
+an_2 = an_1 // div_num
+an_3 = an_2 // div_num
+
+print(f'{an_1} {an_2} {an_3}')
