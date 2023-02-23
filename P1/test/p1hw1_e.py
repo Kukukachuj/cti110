d55ef5136b72eba41a79443fb38784ed53b06d19
Kukukachuj <124608754+Kukukachuj@users.noreply.github.com>
2023-02-23 17:47:40 -0500

    Update

---
+++ P1/test/p1hw1_e.py
@@ -0,0 +1,8 @@
+firstN = int(input('Enter first number\n'))
+secondN = int(input('Enter second number:\n'))
+a_fnsn = firstN + secondN
+m_fnsn = firstN * secondN
+print(firstN)
+print(secondN)
+print(a_fnsn)
+print(m_fnsn)
