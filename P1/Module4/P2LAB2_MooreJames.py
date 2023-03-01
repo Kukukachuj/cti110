65be00606c357bcf58a071a2b9c4771a8f4ae18e
Kukukachuj <124608754+Kukukachuj@users.noreply.github.com>
2023-03-01 05:27:44 -0500

    P2LAB2 Added

---
+++ P1/Module4/P2LAB2_MooreJames.py
@@ -0,0 +1,8 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+product_all=num1*num2*num3*num4
+sum_all=num1+num2+num3+num4
+print("{:.0f} {:.0f}".format(product_all,sum_all/4))
+print("{:.3f} {:.3f}".format(product_all,sum_all/4))
